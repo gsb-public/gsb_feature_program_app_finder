04172fcf503c20c55c76c51d4b771c7163dffe03
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-04-15 16:45:37 -0700

    WP-6993 Rough draft. Pushing for backup

---
+++ templates/gsb-feature-program-app-finder-program-instance-list.tpl.php
@@ -0,0 +1,5 @@
+<div class="program-instance-list">
+  <?php if (!empty($program_instance_list)) : ?>
+    <?php print $program_instance_list; ?>
+  <?php endif; ?>
+</div>
---
+++ templates/gsb-feature-program-app-finder-program-list.tpl.php
@@ -0,0 +1,5 @@
+<div class="program-list">
+  <?php if (!empty($program_list)) : ?>
+    <?php print $program_list; ?>
+  <?php endif; ?>
+</div>
