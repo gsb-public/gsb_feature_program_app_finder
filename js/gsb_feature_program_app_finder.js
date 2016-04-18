01c6ef9f5f97f041967db947d808cbb23ab4326b
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-04-18 15:56:41 -0700

    WP-6993-1 Continued with rough draft

---
+++ js/gsb_feature_program_app_finder.js
@@ -0,0 +1,59 @@
+(function ($) {
+
+  Drupal.behaviors.gsb_feature_program_app_finder = {
+
+    attach: function (context, settings) {
+
+      $('select.program-find-app-select').on('change', function() {
+        Drupal.gsb_feature_program_app_finder.programSelected($(this).val());
+      });
+
+      $('a.programs-different-link').on('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.programs-text').text('');
+        $('select.program-find-app-select').multipleSelect('setSelects', []);
+        $('.ms-parent.program-find-app-select').show();
+        $('div.programs-different').hide();
+        $('.programs-instance-title').hide();
+        $('.programs-instance-deadline').hide();
+        $('.programs-disclaim').show();
+      });
+
+      $('select.program-find-app-select').each(function () {
+        var $select = $(this);
+        $select.multipleSelect({
+          single: true
+        });
+        if (settings.gsb_feature_program_app_finder.program != '') {
+          var p_nid = settings.gsb_feature_program_app_finder.program;
+          $select.val(p_nid);
+          Drupal.gsb_feature_program_app_finder.programSelected(p_nid);
+        }
+      });
+
+    }
+  };
+
+  Drupal.gsb_feature_program_app_finder = Drupal.gsb_feature_program_app_finder || {};
+
+  Drupal.gsb_feature_program_app_finder.programSelected = function(p_nid) {
+    $('.program-instance-list li span').each(function() {
+      var selected_program_nid = $('select.program-find-app-select option:selected').val();
+      var target_id = $(this).attr('data-program-instance-target-id');
+      if (selected_program_nid == target_id) {
+        var instance_title = $(this).attr('data-program-instance-full-title');
+        $('.programs-instance-title').text(instance_title);
+        $('.programs-instance-title').show();
+        var instance_deadline = $(this).attr('data-program-instance-deadline');
+        $('.programs-instance-deadline').text(instance_deadline);
+        $('.programs-instance-deadline').show();
+      }
+    });
+    $('.programs-text').text($('select.program-find-app-select option:selected').text());
+    $('.ms-parent.program-find-app-select').hide();
+    $('.programs-different').show();
+    $('.programs-disclaim').hide();
+  };
+
+})(jQuery);
